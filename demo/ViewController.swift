2056bce18c2c73564133531f279bf286370dc03d
Huang Meng <mark.huang1212@gmail.com>
2021-11-22 21:39:13 +0800

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Meng on 22/11/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
